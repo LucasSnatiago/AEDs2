81846dabbd196917dc945c91c222d3dd7fe224e7
Lucas Santiago <lu.santi.oli@gmail.com>
2019-09-09 12:20:57 -0300

    Listas

---
+++ Tp2/Java/Times.java
@@ -0,0 +1,14 @@
+public class Times {
+
+}
+
+class Time {
+    private:
+    
+    private String nomeTime, apelidoTime, nomeEstadio, tecnico,
+                    capacidade, data;
+    private long tamanhoArquivo;
+
+    public:
+
+}
